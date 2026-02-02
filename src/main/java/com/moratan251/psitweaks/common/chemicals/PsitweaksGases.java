8ae9d72fb1cee8a498ceb851da63480c243994ac
moratan251 <htv432ru@gmail.com>
2026-02-03 03:17:03 +0900

    0.7.0 サイオニックガス関連WIP

--- src/main/java/com/moratan251/psitweaks/common/chemicals/PsitweaksGases.java
+++ src/main/java/com/moratan251/psitweaks/common/chemicals/PsitweaksGases.java
@@ -2,7 +2,10 @@ package com.moratan251.psitweaks.common.chemicals;
 
 
 import mekanism.api.chemical.gas.Gas;
+import mekanism.api.chemical.gas.GasBuilder;
+import mekanism.api.chemical.gas.attribute.GasAttributes;
 import mekanism.api.chemical.infuse.InfuseType;
+import mekanism.api.math.FloatingLong;
 import mekanism.common.registration.impl.GasDeferredRegister;
 import mekanism.common.registration.impl.GasRegistryObject;
 import mekanism.common.registration.impl.InfuseTypeDeferredRegister;
@@ -17,8 +20,19 @@ public class PsitweaksGases {
 
     public static final GasRegistryObject<Gas> PSIONIC_ECHO =
             GASES.register("gas_psionic_echo", 0x40006e);
+
     public static final GasRegistryObject<Gas> PEO_FUEL =
-            GASES.register("gas_peo_fuel", 0x4d749b);
+            GASES.register("gas_peo_fuel", () -> new Gas(GasBuilder.builder()
+                    .tint(0x4d749b)
+                    .with(new GasAttributes.Fuel(
+                            1,    // burnTicks: 1mBあたりの燃焼tick数
+                            FloatingLong.create(64000)  // energyDensity: 1mBあたりの総エネルギー量（FE）
+                    ))
+            ));
+
+
+
+
 
 
     public static void register(IEventBus eventBus) {
