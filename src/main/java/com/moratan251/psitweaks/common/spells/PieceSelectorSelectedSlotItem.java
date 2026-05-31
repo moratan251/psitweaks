728d23c7c25dfd28865320f4641720cc1526f975
moratan251 <htv432ru@gmail.com>
2026-05-31 23:12:42 +0900

    nbt value パス対応

---
+++ src/main/java/com/moratan251/psitweaks/common/spells/PieceSelectorSelectedSlotItem.java
@@ -0,0 +1,38 @@
+package com.moratan251.psitweaks.common.spells;
+
+import com.moratan251.psitweaks.common.spells.item.EntityInventorySlotSource;
+import com.moratan251.psitweaks.common.spells.item.SpellItemValue;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+import vazkii.psi.api.spell.Spell;
+import vazkii.psi.api.spell.SpellContext;
+import vazkii.psi.api.spell.SpellRuntimeException;
+import vazkii.psi.api.spell.piece.PieceSelector;
+
+public class PieceSelectorSelectedSlotItem extends PieceSelector {
+    public PieceSelectorSelectedSlotItem(Spell spell) {
+        super(spell);
+    }
+
+    @Override
+    public Object execute(SpellContext context) throws SpellRuntimeException {
+        int slot = context.getTargetSlot();
+        ItemStack stack = context.caster.getInventory().getItem(slot);
+        if (stack.isEmpty()) {
+            return SpellItemValue.EMPTY;
+        }
+        return SpellItemValue.sourced(
+                stack,
+                new EntityInventorySlotSource(context.caster.getUUID(), context.caster.getId(), slot));
+    }
+
+    @Override
+    public Class<?> getEvaluationType() {
+        return SpellItemValue.class;
+    }
+
+    @Override
+    public Component getEvaluationTypeString() {
+        return Component.translatable("psitweaks.datatype.item");
+    }
+}
